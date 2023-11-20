512ec9a8da89cec7fd7704b51765a4f4b21bd866
Micah Gorrell <micah.gorrell@venafi.com>
2023-11-20 15:15:36 -0700

    Simplifying for now

--- flake.nix
+++ flake.nix
@@ -6,21 +6,14 @@
 
 	inputs = {
 		nixpkgs.url							= "github:NixOS/nixpkgs/nixos-unstable";
-		home-manager.url					= "github:nix-community/home-manager";
-		home-manager.inputs.nixpkgs.follows	= "nixpkgs";
 	};
 
-	outputs = inputs@{ nixpkgs, home-manager, ... }: {
+	outputs = inputs@{ nixpkgs, ... }: {
 		nixosConfigurations = {
 			"lord" = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
 				modules = [
 					./configuration.nix
-
-					home-manager.nixosModules.home-manager {
-						home-manager.useGlobalPkgs		= true;
-						home-manager.useUserPackages	= true;
-					}
 				];
 			};
 		};
