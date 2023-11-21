029d7b634f77c231c6757d223f57d2a2843e27a4
Micah Gorrell <micah.gorrell@venafi.com>
2023-11-20 19:23:09 -0700

    Cleanup

--- configuration.nix
+++ configuration.nix
@@ -12,204 +12,179 @@ in
 		./hardware-configuration.nix
     ];
 
-  # Automatic Upgrades
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = false;
+	# Enable the nix command and flakes
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	# Allow unfree packages
+	nixpkgs.config.allowUnfree = true;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	security.sudo.wheelNeedsPassword = false;
 
-  boot.initrd.luks.devices."luks-7c93fd91-b48e-49bb-9de9-28832248b424".device = "/dev/disk/by-uuid/7c93fd91-b48e-49bb-9de9-28832248b424";
-  networking.hostName = "lord"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	# Automatic Upgrades
+	system.autoUpgrade.enable = true;
+	system.autoUpgrade.allowReboot = false;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	# Bootloader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  programs.nm-applet.enable = true;
+	boot.initrd.luks.devices."luks-7c93fd91-b48e-49bb-9de9-28832248b424".device = "/dev/disk/by-uuid/7c93fd91-b48e-49bb-9de9-28832248b424";
+	networking.hostName = "lord"; # Define your hostname.
+	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Set your time zone.
-  time.timeZone = "America/Denver";
+	# Configure network proxy if necessary
+	# networking.proxy.default = "http://user:password@proxy:port/";
+	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+	# Enable networking
+	networking.networkmanager.enable = true;
+	programs.nm-applet.enable = true;
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+	# Set your time zone.
+	time.timeZone = "America/Denver";
+
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
+
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS			= "en_US.UTF-8";
+		LC_IDENTIFICATION	= "en_US.UTF-8";
+		LC_MEASUREMENT		= "en_US.UTF-8";
+		LC_MONETARY			= "en_US.UTF-8";
+		LC_NAME				= "en_US.UTF-8";
+		LC_NUMERIC			= "en_US.UTF-8";
+		LC_PAPER			= "en_US.UTF-8";
+		LC_TELEPHONE		= "en_US.UTF-8";
+		LC_TIME				= "en_US.UTF-8";
+	};
 
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+	# Enable CUPS to print documents.
+	services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+	# Enable sound with pipewire.
+	sound.enable				= true;
+	hardware.pulseaudio.enable	= false;
+	security.rtkit.enable		= true;
+	services.pipewire = {
+		enable					= true;
+		alsa.enable				= true;
+		alsa.support32Bit		= true;
+		pulse.enable			= true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+		# jack.enable			= true;
+	};
 
-  programs = {
-    zsh = {
-      enable = true;
-      interactiveShellInit = ''
-        source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-      '';
-    };
-  };
+	programs.zsh = {
+		enable = true;
+		interactiveShellInit = ''
+			source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+		'';
+	};
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.m = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    description = "Micah N Gorrell";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      zsh
-      firefox-wayland
-      tridactyl-native
-      thunderbird
-      kitty
-      neofetch
-      acpi
-      starship
-      codespell
-      mdcat
-      ripgrep
-      eza
-      unzip
-      bemenu
-      wdisplays
-      slack
-      bitwarden
-      steam
-    ];
-  };
+	# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.m = {
+		isNormalUser	= true;
+		shell			= pkgs.zsh;
+		description		= "Micah N Gorrell";
+		extraGroups		= [ "networkmanager" "wheel" ];
 
-  environment.shellAliases = {
-    vi = "nvim";
-  };
+		packages = with pkgs; [
+			zsh
+			firefox-wayland
+			tridactyl-native
+			thunderbird
+			kitty
+			neofetch
+			acpi
+			starship
+			codespell
+			mdcat
+			ripgrep
+			eza
+			unzip
+			bemenu
+			wdisplays
+			slack
+			bitwarden
+			steam
+		];
+	};
 
-  security.sudo.wheelNeedsPassword = false;
+	environment.shellAliases = {
+		vi = "nvim";
+	};
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = [
-    pkgs.zsh
-    pkgs.psmisc
-    pkgs.zsh-syntax-highlighting
-    pkgs.zsh-vi-mode
-    pkgs.git
-	pkgs.gnumake
-    pkgs.neovim
-    pkgs.go
-    pkgs.curl
-    pkgs.stow
-    pkgs.xdg-utils
-    pkgs.wayland
-    pkgs.gbar
-    pkgs.hyprpaper
-	dwl.packages.${pkgs.system}.default
-  ];
-
-  fonts.packages = with pkgs; [
-    nerdfonts
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-  ];
-
-  # Make wayland behave
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-  xdg.portal.wlr.enable = true;
-
-#  services.xserver = {
-#    enable = true;
-#    displayManager.gdm.enable = true;
-#    displayManager.gdm.wayland = true;
-#    libinput.enable = true;
-#  };
-
-# Interception-Tools
-services.interception-tools = {
-	enable = true;
-	plugins = [
-		caps2escPkg
-		mackeysPkg
-		swapmodsPkg
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+	environment.systemPackages = [
+		pkgs.zsh
+		pkgs.psmisc
+		pkgs.zsh-syntax-highlighting
+		pkgs.zsh-vi-mode
+		pkgs.git
+		pkgs.gnumake
+		pkgs.neovim
+		pkgs.go
+		pkgs.curl
+		pkgs.stow
+		pkgs.xdg-utils
+		pkgs.wayland
+		pkgs.gbar
+		pkgs.hyprpaper
+		dwl.packages.${pkgs.system}.default
 	];
-	udevmonConfig = ''
+
+	fonts.packages = with pkgs; [
+		nerdfonts
+		noto-fonts
+		noto-fonts-cjk
+		noto-fonts-emoji
+		liberation_ttf
+		fira-code
+		fira-code-symbols
+		mplus-outline-fonts.githubRelease
+		dina-font
+		proggyfonts
+	];
+
+	# Make wayland behave
+	environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+	xdg.portal.wlr.enable = true;
+
+	# Interception-Tools
+	services.interception-tools = {
+		enable = true;
+		plugins = [
+			caps2escPkg
+			mackeysPkg
+			swapmodsPkg
+		];
+		udevmonConfig = ''
 - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${swapmodsPkg}/bin/swapmods | ${mackeysPkg}/bin/mackeys | ${caps2escPkg}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
   DEVICE:
     NAME: AT Translated Set 2 keyboard
 - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${mackeysPkg}/bin/mackeys | ${caps2escPkg}/bin/caps2esc -m 1 | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
   DEVICE:
     NAME: ".*((k|K)(eyboard|EYBOARD)).*"
-	'';
-};
+		'';
+	};
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+	# Enable the OpenSSH daemon.
+	services.openssh.enable = true;
 
-  # List services that you want to enable:
+	# Open ports in the firewall.
+	# networking.firewall.allowedTCPPorts = [ ... ];
+	# networking.firewall.allowedUDPPorts = [ ... ];
+	# Or disable the firewall altogether.
+	# networking.firewall.enable = false;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "23.05"; # Did you read the comment?
 }
