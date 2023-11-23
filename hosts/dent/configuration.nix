c007bcddc27cabcb1ee7bc01bff2f93f396ff7fe
Micah Gorrell <micah.gorrell@venafi.com>
2023-11-23 14:54:34 -0700

    Added amdgpu settings for dent

--- hosts/dent/configuration.nix
+++ hosts/dent/configuration.nix
@@ -5,6 +5,26 @@
 		./hardware-configuration.nix
 	];
 
+	# amdgpu
+	boot.initrd.kernelModules = [ "amdgpu" ];
+	hardware.opengl.extraPackages = with pkgs; [
+		rocm-opencl-icd
+		rocm-opencl-runtime
+		amdvlk
+	];
+
+	hardware.opengl.extraPackages32 = with pkgs; [
+		driversi686Linux.amdvlk
+	];
+
+	hardware.opengl.driSupport = true;
+	hardware.opengl.driSupport32Bit = true;
+
+	environment.systemPackages = with pkgs; [
+		clinfo
+		blender-hip
+	];
+
 	networking.hostName = "dent";
 	time.timeZone = "America/Denver";
 }
