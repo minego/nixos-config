35b1a0b3959562bcd73cbcd38a62bbebb2989a9a
Micah Gorrell <micah.gorrell@venafi.com>
2023-11-25 08:40:55 -0700

    Added an input for home manager

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+		home-manager = {
+			url = github:nix-community/home-manager;
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
 	outputs = {
