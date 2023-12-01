e77285290714210dbc0a2b92bc4527e6a9065808
Micah N Gorrell <m@minego.net>
2023-11-30 19:31:49 -0700

    Now using home-manager on lord as well

--- hosts/lord/default.nix
+++ hosts/lord/default.nix
@@ -3,7 +3,6 @@
 let
 	pkgs	= inputs.nixpkgs;
 	lib		= inputs.nixpkgs.lib;
-	config	= inputs.config;
 in
 lib.nixosSystem {
 	system = "x86_64-linux";
@@ -30,19 +29,6 @@ lib.nixosSystem {
 			boot.initrd.luks.devices."luks-7c93fd91-b48e-49bb-9de9-28832248b424".device = "/dev/disk/by-uuid/7c93fd91-b48e-49bb-9de9-28832248b424";
 
 			services.fstrim.enable = lib.mkDefault true;
-
-			boot = rec {
-				kernelParams = [
-					# Disable "Panel Self Refresh".  Fix random freezes on the X270.
-					"i915.enable_psr=0"
-				];
-
-				# acpi_call is needed for tlp to work properly on the thinkpad
-				kernelModules = [ "acpi_call" ];
-				extraModulePackages = with config.boot.kernelPackages; [
-					acpi_call
-				];
-			};
 		}
 	];
 }
--- hosts/lord/hardware-configuration.nix
+++ hosts/lord/hardware-configuration.nix
@@ -4,41 +4,39 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+	imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+	boot = {
+		initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+		initrd.kernelModules = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/8a13eb1e-ddfa-48ac-a5df-94f3f806f804";
-      fsType = "ext4";
-    };
+		# acpi_call is needed for tlp to work properly on the thinkpad
+		kernelModules = [ "kvm-intel" "acpi_call" ];
 
-  boot.initrd.luks.devices."luks-37e66e63-a8bd-43c3-9148-a02cc71c5413".device = "/dev/disk/by-uuid/37e66e63-a8bd-43c3-9148-a02cc71c5413";
+		kernelParams = [
+			# Disable "Panel Self Refresh".  Fix random freezes on the X270.
+			"i915.enable_psr=0"
+		];
+		extraModulePackages = with config.boot.kernelPackages; [
+			acpi_call
+		];
+	};
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1B70-CFE3";
-      fsType = "vfat";
-    };
+	fileSystems = {
+		"/" = {
+			device = "/dev/disk/by-uuid/8a13eb1e-ddfa-48ac-a5df-94f3f806f804";
+			fsType = "ext4";
+		};
+		"/boot" = {
+			device = "/dev/disk/by-uuid/1B70-CFE3";
+			fsType = "vfat";
+		};
+	};
+	boot.initrd.luks.devices."luks-37e66e63-a8bd-43c3-9148-a02cc71c5413".device = "/dev/disk/by-uuid/37e66e63-a8bd-43c3-9148-a02cc71c5413";
+	swapDevices = [ { device = "/dev/disk/by-uuid/f3ae26c1-3fcc-41c4-aa31-377bef39223c"; } ];
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/f3ae26c1-3fcc-41c4-aa31-377bef39223c"; }
-    ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wwp0s20f0u3i12.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+	networking.useDHCP = lib.mkDefault true;
+	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+	powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+	hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
