d22628c4437fd6c638b8a0dc0723efa68022affa
Micah N Gorrell <m@minego.net>
2023-12-05 16:34:32 -0700

    Okay, firefox should now be working

--- users/m/modules/firefox/default.nix
+++ users/m/modules/firefox/default.nix
@@ -34,6 +34,45 @@ with lib;
 				name			= "default";
 				isDefault		= true;
 
+				search = {
+					default		= "Google";
+					force		= true;
+					order = [
+						"Google"
+						"Nix Packages"
+						"NixOS Wiki"
+					];
+
+					engines = {
+						"Nix Packages" = {
+							urls = [{
+								template = "https://search.nixos.org/packages";
+								params = [
+									{ name = "type"; value = "packages"; }
+									{ name = "query"; value = "{searchTerms}"; }
+								];
+							}];
+
+							icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+							definedAliases = [ "@np" ];
+						};
+
+						"NixOS Wiki" = {
+							urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+
+							iconUpdateURL = "https://nixos.wiki/favicon.png";
+							updateInterval = 24 * 60 * 60 * 1000; # every day
+							definedAliases = [ "@nw" ];
+						};
+
+						"Bing".metaData.hidden = true;
+						"Twitter".metaData.hidden = true;
+						"Facebook".metaData.hidden = true;
+
+						"Google".metaData.alias = "@g";
+					};
+				};
+
 				extensions = with pkgs.nur.repos.rycee.firefox-addons; [
 					bitwarden
 					tridactyl
