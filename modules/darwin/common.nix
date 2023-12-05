9aa0b35175ef30ba2b2fa68d31e1ca15c5c607d3
Micah N Gorrell <m@minego.net>
2023-12-05 13:18:13 -0700

    Minor tweaks for darwin

--- modules/darwin/common.nix
+++ modules/darwin/common.nix
@@ -1,7 +1,16 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
+	environment = {
+		shells			= with pkgs; [ bash zsh ];
+		loginShell		= pkgs.zsh;
+		systemPackages	= [ pkgs.coreutils ];
+		systemPath		= [ "/opt/homebrew/bin" ];
+		pathsToLink		= [ "/Applications" ];
+	};
 
+	# Do this with Karabiner instead
+	system.keyboard.enableKeyMapping = false;
 
 	# Don't touch
 	system.stateVersion = 4;
