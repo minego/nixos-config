37ece4b4ab2c4f33841efeb158f9cbc7f557eeda
Micah N Gorrell <m@minego.net>
2024-01-03 12:12:52 -0700

    Updating syncthing config to add a shared source directory and to add additional hosts that I build on

--- modules/linux/syncthing.nix
+++ modules/linux/syncthing.nix
@@ -16,6 +16,8 @@
 				"zaphod2"	= { id = "HAS6HSV-BOM63S2-YJMIQNC-COLZMBU-F2BPIL7-ULY3AL7-CERGJEY-BHAXUQX"; };
 				"lord"		= { id = "GFOMJN4-SPH4YOO-36L2UQM-22FNFKN-AWFT5YT-C5AHAUK-7CGH4MF-Q3BA4QJ"; };
 				"pixel7"	= { id = "L4KCXPG-5LK5DFR-I3RSZOD-T43RKOG-GKU7HTK-QSMRU33-TIQP3TX-EKO2YQN"; };
+				"zem"		= { id = "TFBJXJ5-XKVNFHI-HVJ5X3N-ATR3XKF-EIKXFLQ-C6XX4K5-UDZUPS7-OJ2TXQF"; };
+				"random"	= { id = ""; };
 			};
 
 			folders = {
@@ -23,9 +25,23 @@
 					enable	 = true;
 					id		= "notes";
 					path	= "/home/m/notes";
-					devices	= [ "dent" "lord" "pixel7" "zaphod2" ];
+					devices	= [ "dent" "lord" "pixel7" "zaphod2" "zem" "random" ];
+				};
+
+				"Code" = {
+					enable	 = true;
+					id		= "notes";
+					path	= "/home/m/src/shared";
+					devices	= [ "dent" "lord" "pixel7" "zaphod2" "zem" "random" ];
 				};
 			};
 		};
 	};
+
+	# Syncthing ports:
+	#	8384/TCP			Remote access to GUI - Do NOT open here
+	#	22000/TCP+UDP		Sync traffic
+	#	21027/UDP			Discovery
+	networking.firewall.allowedTCPPorts = [ 22000 ];
+	networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
