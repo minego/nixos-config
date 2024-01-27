44b1cbcf433c635bde9b2031b1edd4d6d45fdfe8
Micah N Gorrell <m@minego.net>
2024-01-27 09:22:31 -0700

    Updated and re-enabled extra bluetooth codecs now that doing so is working on aarch64

--- modules/linux/common.nix
+++ modules/linux/common.nix
@@ -59,19 +59,17 @@ with lib;
 			# jack.enable					= true;
 		};
 
-	# TODO Figure out why this isn't working on aarch64
-	#
-	#	# Enable additional bluetooth codecs
-	#	environment.etc = {
-	#		"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-	#			bluez_monitor.properties = {
-	#				["bluez5.enable-sbc-xq"] = true,
-	#				["bluez5.enable-msbc"] = true,
-	#				["bluez5.enable-hw-volume"] = true,
-	#				["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-	#			}
-	#		'';
-	#	};
+		# Enable additional bluetooth codecs
+		environment.etc = {
+			"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+				bluez_monitor.properties = {
+					["bluez5.enable-sbc-xq"] = true,
+					["bluez5.enable-msbc"] = true,
+					["bluez5.enable-hw-volume"] = true,
+					["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+				}
+			'';
+		};
 
 		systemd.user.services.mpris-proxy = {
 			description = "Mpris proxy";
