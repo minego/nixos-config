412972507c5352cc2fc3fa34bfd9edb3839e447c
Micah N Gorrell <m@minego.net>
2024-02-26 09:58:02 -0700

    Update and enable upower on my laptop

--- modules/linux/laptop.nix
+++ modules/linux/laptop.nix
@@ -8,7 +8,12 @@ with lib;
 
 	# Don't shutdown if someone hits the power button!
 	services.logind = {
-		extraConfig		= "HandlePowerKey=suspend";
-		lidSwitch		= "suspend";
+		extraConfig			= "HandlePowerKey=suspend";
+		lidSwitch			= "suspend";
+	};
+
+	services.upower = {
+		enable				= true;
+		criticalPowerAction	= "Hibernate";
 	};
 }
